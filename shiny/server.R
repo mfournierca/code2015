578eaa1ef40751a1e22ce38d7e0947673df29b25
Matthew Fournier <fournier.matthew@gmail.com>
2015-04-01 09:09:03 -0400

    add image generation to shiny server
    Issue: convert_to_shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,8 +1,14 @@
 library(shiny)
 library(dplyr)
+library(ggplot2)
 
-cip <- read.csv("../data/cip_mapping.csv")
-df <- read.csv("../data/dump.csv")
+cipdf <- read.csv("../data/cip_mapping.csv")
+nocdf <- read.csv("../data/noc_mapping.csv")
+datadf <- read.csv("../data/dump.csv")
+
+colnames(nocdf)[which(names(nocdf) == "category_key")] <- "NOC2011"
+
+head(datadf)
 
 # Define a server for the Shiny app
 function(input, output) {
@@ -10,12 +16,22 @@ function(input, output) {
   output$nocPlot <- renderPlot({
     
     cip <- input$cip
-    cip_key <- cip[cip$category_name==s, "category_key"]
+    cip_key <- cipdf[cipdf$category_name==cip, "category_key"]
+
+    p <- datadf %>% 
+      filter(CIP2011_4 == cip_key, !is.na(occupation_cat4)) %>% 
+      arrange(desc(observation_value)) %>% 
+      slice(1:25) %>%
+      inner_join(nocdf, by="NOC2011") %>%
+      select(observation_value, category_name) %>%
+      ggplot(aes(x=category_name, y=observation_value)) + 
+        geom_bar(stat="identity") +
+        theme(axis.text.x = element_text(angle = 30, hjust = 1))
     
-    print(cip_key)
-    
+    print(p)
   })
 }
+
 
 
 
