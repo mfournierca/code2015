a0f0d82d2661bb367fbe5be7c206680580e05e71
Matthew Fournier <fournier.matthew@gmail.com>
2015-04-09 17:52:42 -0400

    sort x axis of plot by observation value, not alphabetically
    Issue: convert_to_shiny

--- shiny/server.R
+++ shiny/server.R
@@ -12,24 +12,41 @@ head(datadf)
 
 # Define a server for the Shiny app
 function(input, output) {
-  
-  output$nocPlot <- renderPlot({
-    
-    cip <- input$cip
-    cip_key <- cipdf[cipdf$category_name==cip, "category_key"]
 
-    p <- datadf %>% 
-      filter(CIP2011_4 == cip_key, !is.na(occupation_cat4)) %>% 
-      arrange(desc(observation_value)) %>% 
-      slice(1:25) %>%
-      inner_join(nocdf, by="NOC2011") %>%
-      select(observation_value, category_name) %>%
-      ggplot(aes(x=category_name, y=observation_value)) + 
+    output$nocPlot <- renderPlot({
+
+        cip <- input$cip
+        cip_key <- cipdf[cipdf$category_name==cip, "category_key"]
+
+        p <- datadf %>% 
+            # to avoid selecting top level categories, filter on cat4 
+            filter(CIP2011_4 == cip_key, !is.na(occupation_cat4)) %>%
+            
+            # sort by observation value descending 
+            arrange(desc(observation_value)) %>% 
+            
+            # take the top 
+            slice(1:10) %>%
+            
+            # inner join to pick up the occupation name
+            inner_join(nocdf, by="NOC2011") %>%
+
+            # discard unused columns
+            select(observation_value, category_name) 
+       
+        # debug
+        print(p)
+
+        # prevent ggplot from ordering the x axis alphabetically
+        p$category_name <- factor(p$category_name, levels=p$category_name, ordered=TRUE)
+
+        # plot
+        p %>% ggplot(aes(x=category_name, y=observation_value)) + 
         geom_bar(stat="identity") +
         theme(axis.text.x = element_text(angle = 30, hjust = 1))
     
-    print(p)
-  })
+
+    })
 }
 
 
