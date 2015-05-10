a0593817935ff2f432082f7ca281abf7a85adf14
Matthew Fournier <fournier.matthew@gmail.com>
2015-05-10 13:31:33 -0400

    add explanation text to ui.R
    Issue: master

--- shiny/ui.R
+++ shiny/ui.R
@@ -8,30 +8,41 @@ cip <- cip[grep("^[1234567890]", cip$category_name), ]
 fluidPage(      
     
     # Give the page a title
-    titlePanel("Occupation by Education Category"),
-    
-    # Generate a row with a sidebar
-    sidebarLayout(      
-      
-        # Define the sidebar with one input
-        sidebarPanel(
-            selectInput(
-                "cip", 
-                "Field of Education: ", 
-                choices=as.vector(cip$category_name),
-                width="100%"
+    titlePanel("Employment Categories by Education"),
+    fluidRow( 
+        # Generate a row with a sidebar
+        sidebarLayout(      
+          
+            # Define the sidebar with one input
+            sidebarPanel(
+                 selectInput(
+                    "cip", 
+                    "Select Field of Education: ", 
+                    choices=as.vector(cip$category_name),
+                    width="100%",
+                ), 
+                helpText("This tool generates a graph of where people with a given education are employed. The graph is ranked by number of people in each job, so the most popular / likely occupations are first."),
+                # caveats / notes
+                HTML("
+                    <p>Notes:</p>
+                    <ul>
+                        <li>Data taken from the 2011 Canadian national census.</li>
+                        <li>Values are estimates for the total population based on the census.</li>
+                        <li>Data is aggregated across all age groups, provinces and census divisions.</li>
+                        <li>Some data is excluded due to quality issues, privacy rules imposed on Statistics Canada, or non-response on the census.</li>
+                    </ul>"
+                )
             ),
-            hr(),
-            helpText("Select a field of education.")
-        ),
 
-        # Create a spot for the barplot
-        mainPanel(
-            plotOutput("nocPlot")  
-        ),
+            # Create a spot for the barplot
+            mainPanel(
+                plotOutput("nocPlot")  
+            ),
 
-        fluid=TRUE
-    )
+            fluid=TRUE
+        )
+    ), 
+    hr()
 )
 
 
